75fe2bec9132852d5039f1781e354aeeef1ce8d5
Ozgur Ersoz <ozgur.ersoz3@gmail.com>
2021-01-20 23:51:53 +0300

    day 4 - 20.01

---
+++ Collections/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] isimler = new string[] { "Engin", "Murat", "Kerem", "Halil" };
+
+            //Console.WriteLine(isimler[0]);
+            //Console.WriteLine(isimler[1]);
+            //Console.WriteLine(isimler[2]);
+            //Console.WriteLine(isimler[3]);
+
+            //isimler = new string[5];  //yeni referans adresi yaratildi
+            //isimler[4] = "ilker";
+            //Console.WriteLine(isimler[4]);
+            //Console.WriteLine(isimler[0]); //yaratilan yeni adresin 5. elemani disinda elemani yok
+
+            //bu nedenle diziler yerine koleksiyon yapilarti kullanilir
+            //List<string> isimler2 = new List<string>();
+            //isimler2.Add("Engin");
+
+            List<string> isimler2 = new List<string> { "Engin", "Murat", "Kerem", "Halil" };
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+            Console.WriteLine(isimler2[3]);
+            isimler2.Add("Ilker");
+            Console.WriteLine(isimler2[4]);
+            Console.WriteLine(isimler2[0]);
+
+            isimler2.Clear();
+                
+
+        }
+
+    }
+}
---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class ProductManager
+    {
+        //Encapsulation
+        public void Add(Product product)
+        {
+            Console.WriteLine(product.ProductName + " eklendi");
+        }
+
+        public void Update(Product product)
+        {
+            Console.WriteLine(product.ProductName + " guncellendi");
+        }
+
+
+        public int topla(int sayi1, int sayi2)
+        {
+          return sayi1 + sayi2;
+        }
+        public void Topla2(int sayi1, int sayi2)
+        {
+            Console.WriteLine(sayi1 + sayi2);
+        }
+
+
+
+    }
+}
